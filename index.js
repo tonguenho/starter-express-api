062aa2537076a5baa8ab4a9ae910791b1fc2db18
Christian Tong <christiantong5@gmail.com>
2023-07-07 14:49:58 -0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+
+let messages = [];
+
+app.get('/', (request, response) => {
+    let message = messages[0];
+    messages.shift();
+    response.send(message);
+});
+
+app.post('/', (request, response) => {
+    let message = request.body.message;
+    if(messages.length <= 200){
+        messages.push(message);
+        response.send("Ok");
+    }else{
+        response.send("O limite de mensagens no servidor foi atingido, tente novamente mais tarde.");
+    }
+    
 })
 app.listen(process.env.PORT || 3000)
